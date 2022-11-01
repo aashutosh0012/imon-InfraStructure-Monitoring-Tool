1c70a44b791cc0975d1133daa4874502b02b1fa4
Aashutosh Kumar <aashutosh0012@gmail.com>
2022-11-01 21:32:35 +0530

    Update settings.py

--- imon/settings.py
+++ imon/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kml@a-j70cu!-xj!h-qmjj=e!&s7=1zkxv6b7c@$^lxivr++c*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
